55aa8cddd4099253fa31b25ec5f8d9e5958ce500
djavani <djavanirgomes@gmail.com>
2018-02-26 23:41:56 -0300

    more complex List Items

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -7,7 +7,7 @@ import { FavoritesPage } from '../favorites/favorites';
 @Component({
     selector: "page-tabs",
     template: `
-        <ion-tabs selectedIndex="0">
+        <ion-tabs selectedIndex="1">
         <ion-tab [root]="favoritesPage" fabTitle="Favorites" tabIcon="star"></ion-tab>
         <ion-tab [root]="libraryPage" fabTitle="Library" tabIcon="book"></ion-tab>
         </ion-tabs>
